6293bd48c07209bf654a7955b2d77f8c63ddf2fe
shixin <shixin1359@gmail.com>
2021-03-12 15:57:05 +0800

    重构ProtectBee和ThunderBackfire

---
+++ ProtectBee/app/src/main/java/com/example/administrator/mylovegame/GameMusic.java
@@ -0,0 +1,75 @@
+package com.example.administrator.mylovegame;
+
+import java.io.IOException;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class GameMusic extends Service {
+    private MediaPlayer mp;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mp.start();
+        // 音乐播放完毕的事件处理
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            public void onCompletion(MediaPlayer mp) {
+                // 循环播放
+                try {
+                    mp.start();
+                } catch (IllegalStateException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        // 播放音乐时发生错误的事件处理
+        mp.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                // 释放资源
+                try {
+                    mp.release();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                return false;
+            }
+        });
+        return super.onStartCommand(intent, flags, startId);
+    }
+    @Override
+    public void onCreate() {
+        // 初始化音乐资源
+        try {
+            // 创建MediaPlayer对象
+            mp = new MediaPlayer();
+            // 将音乐保存在res/raw/xingshu.mp3,R.java中自动生成{public static final int xingshu=0x7f040000;}
+            mp = MediaPlayer.create(GameMusic.this, R.raw.abxn);
+            // 在MediaPlayer取得播放资源与stop()之后要准备PlayBack的状态前一定要使用MediaPlayer.prepeare()
+            mp.prepare();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        // 服务停止时停止播放音乐并释放资源
+        mp.stop();
+        mp.release();
+
+        super.onDestroy();
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
---
+++ ProtectBee/app/src/main/java/com/example/administrator/mylovegame/IGameImage.java
@@ -0,0 +1,18 @@
+package com.example.administrator.mylovegame;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Administrator on 2016/12/21 0021.
+ */
+public interface IGameImage {
+    Bitmap getBitmap();
+
+    int getX();
+
+    int getY();
+
+    int getWidth();
+
+    int getHeight();
+}
